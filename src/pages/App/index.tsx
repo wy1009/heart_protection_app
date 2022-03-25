9c2b7647fc1e8e82945312fa1da4f612432076fd
Yi Wang <wy941009@gmail.com>
2022-03-25 21:08:56 +0800

    feat: 更改结构，增加页面

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ScrollView, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Text>test</Text>
+    </ScrollView>
+  )
+}
+
+export default App
